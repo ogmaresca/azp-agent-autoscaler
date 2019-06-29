acf1249add2370dfbd45b0c2c997447f9710211a
ggodone-maresca <ggodonemaresca@gmail.com>
2019-06-28 20:42:21 -0400

    Fill out arguments

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"strconv"
+	"time"
 
 	//"strings"
 	//"time"
@@ -15,23 +17,58 @@ import (
 func main() {
 	// Parse arguments
 	minStr := flag.String("min", "1", "Minimum number of free agents to keep alive. Minimum of 1.")
+	rateStr := flag.String("rate", "10s", "Duration to check the number of agents.")
+	resourceType := flag.String("type", "StatefulSet", "Resource type of the agent. Only StatefulSet is supported.")
+	resourceName := flag.String("name", "", "The name of the StatefulSet.")
+	resourceNamespace := flag.String("namespace", "", "The namespace of the StatefulSet.")
+	azpToken := flag.String("token", "", "The Azure Devops token.")
 
 	flag.Parse()
 
 	min, err := strconv.Atoi(*minStr)
-
 	if err != nil {
-		panic("Error converting --min argument to int: " + err.Error())
+		panic(fmt.Sprintf("Error converting min argument to int: %s", err.Error()))
 	} else if min < 1 {
-		panic("Error - --min argument cannot be less than 1.")
+		panic("Error - min argument cannot be less than 1.")
+	}
+
+	rate, err := time.ParseDuration(*rateStr)
+	if err != nil {
+		panic(fmt.Sprintf("Error parsing rate: %s", err.Error()))
+	} else if rate.Seconds() <= 1 {
+		panic(fmt.Sprintf("Error - rate '%s' is too low.", rate.String()))
+	}
+
+	if *resourceType != "StatefulSet" {
+		panic(fmt.Sprintf("Error - Unknown resource type %s", *resourceType))
+	}
+
+	if *resourceName == "" {
+		panic(fmt.Sprintf("Error - %s name is required.", *resourceType))
+	}
+
+	if *resourceNamespace == "" {
+		panic("Error - namespace is required.")
+	}
+
+	if *azpToken == "" {
+		panic("Error - the Azure Devops token is required.")
 	}
 
 	// Initialize Kubernetes client
+	/*k8sConfig, err := clientcmd.BuildConfigFromFlags("", "")
+	if err != nil {
+		panic("Error initializing Kubernetes config: " + err.Error())
+	}*/
 	k8sConfig, err := rest.InClusterConfig()
 	if err != nil {
-		k8sConfig, err = clientcmd.BuildConfigFromFlags("", home()+"/.kube")
+		kubeconfigEnv := os.Getenv("KUBECONFIG")
+		k8sConfig, err = clientcmd.BuildConfigFromFlags("", kubeconfigEnv)
 		if err != nil {
-			panic("Error initializing Kubernetes config: " + err.Error())
+			k8sConfig, err = clientcmd.BuildConfigFromFlags("", fmt.Sprintf("%s/.kube/config", homepath()))
+			if err != nil {
+				panic("Error initializing Kubernetes config: " + err.Error())
+			}
 		}
 	}
 
@@ -39,9 +76,15 @@ func main() {
 	if err != nil || k8sClient == nil {
 		panic("Error initializing Kubernetes config: " + err.Error())
 	}
+
+	for {
+		time.Sleep(rate)
+	}
+
+	println("Exiting azp-agent-autoscaler")
 }
 
-func home() string {
+func homepath() string {
 	home := os.Getenv("HOME")
 	if home != "" {
 		return home
